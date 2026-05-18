5290bae0621a589063d10f3c6097c7786ec67ec2
Aenri Lovehart <aenri@loveh.art>
2026-05-18 01:00:50 -0400

    feat(security): set io_uring to 1

--- modules/kernel/config/common.nix
+++ modules/kernel/config/common.nix
@@ -52,6 +52,7 @@
     "kernel.unprivileged_userns_clone" = 1;
     "kernel.apparmor_restrict_unprivileged_userns" = 1;
     "kernel.apparmor_restrict_unprivileged_unconfined" = 1;
+    "kernel.io_uring" = 1;
     "kernel.perf_event_paranoid" = 3;
     "kernel.yama.ptrace_scope" = 1;
     "kernel.unprivileged_bpf_disabled" = 1;
